c35e7f6c46ff39665379664950384c05378a806d
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-25 00:55:05 +0530

    Added:welcome

---
+++ src/main/java/bridelabz/Userregistration_junit_testcases/Userregistration.java
@@ -0,0 +1,10 @@
+package bridelabz.Userregistration_junit_testcases;
+
+public class Userregistration {
+
+	public static void main(String[] args) {
+		System.out.println("welcome");
+
+	}
+
+}
---
+++ src/test/java/bridelabz/Userregistration_junit_testcases/Userregistrationtest.java
@@ -0,0 +1,19 @@
+package bridelabz.Userregistration_junit_testcases;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test 
+ */
+public class Userregistrationtest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
