8c54e9ad6971bdbf625079bbfa76d652f71715ba
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-25 01:21:49 +0530

    Added:User need to enter a valid Firstname

--- src/main/java/bridelabz/Userregistration_junit_testcases/Userregistration.java
+++ src/main/java/bridelabz/Userregistration_junit_testcases/Userregistration.java
@@ -1,10 +1,18 @@
 package bridelabz.Userregistration_junit_testcases;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Userregistration {
+	/* 
+	 * Created a method to validate first name
+	  */
+	 
 
-	public static void main(String[] args) {
-		System.out.println("welcome");
-
+	 public boolean firstName(String firstName) {
+	        String regex = "^[A-Z]{1}[a-z]{2,}$";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(firstName);
+	        return matcher.matches();
+	    }
 	}
-
-}
--- src/test/java/bridelabz/Userregistration_junit_testcases/Userregistrationtest.java
+++ src/test/java/bridelabz/Userregistration_junit_testcases/Userregistrationtest.java
@@ -1,19 +1,28 @@
 package bridelabz.Userregistration_junit_testcases;
-import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Unit test 
- */
-public class Userregistrationtest
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
+	 * Unit test for validating first name
+	 */
+	public class Userregistrationtest {
+	    Userregistration userRegistration;
+	    @Before
+	    public void before() {
+	        userRegistration = new Userregistration();
+	    }
+
+	    @Test
+	    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+	        boolean result = userRegistration.firstName("Mallepogu");
+	        Assert.assertEquals(true, result);
+	    }
+
+	    @Test
+	    public void givenFirstName_WhenNotProper_ShouldReturnFalse() {
+	        boolean result = userRegistration.firstName("mallepogu");
+	        Assert.assertEquals(false, result);
+	    }
+	}
